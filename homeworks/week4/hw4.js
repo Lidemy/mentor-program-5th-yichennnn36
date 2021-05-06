c6864baee57419ed9d3ee90d248733e28fdab880
yichen <xni830306@gmail.com>
2021-05-06 18:09:29 +0800

    week4_v5

--- homeworks/week4/hw4.js
+++ homeworks/week4/hw4.js
@@ -14,8 +14,12 @@ request({
   if (err) {
     return console.log(err);
   }
-
-  const data = JSON.parse(body);
+  let data;
+  try {
+    data = JSON.parse(body);
+  } catch (err) {
+    return console.log(err);
+  }
   for (let i = 0; i < data.top.length; i++) {
     console.log(`${data.top[i].viewers} ${data.top[i].game.name}`);
   }
