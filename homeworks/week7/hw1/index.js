43537bb8226c5c9ebd0ef9b7429091e136de5ae1
yichen <xni830306@gmail.com>
2021-05-28 14:59:18 +0800

    week7_hw1_finish

--- homeworks/week7/hw1/index.js
+++ homeworks/week7/hw1/index.js
@@ -1,34 +1,40 @@
-/* eslint-disable */
 document.querySelector('form').addEventListener('submit', (e) => {
-  e.preventDefault(); 
+  e.preventDefault();
   let hasErr = false;
-  let res = {};
-  const eles = document.querySelectorAll('.required')
-  for (let ele of eles) {
-    const input = ele.querySelector('.required__input')
-    const radios = ele.querySelectorAll('input[type=radio]')
-    
+  const res = {};
+
+  const inputs = document.querySelectorAll('.required__input');
+  for (const input of inputs) {
     if (input) {
-      res[input.name] = input.value
-      if (!input.value) {
-        ele.classList.remove('hide__err');
-        hasErr = true;
-      } else {
-        ele.classList.add('hide__err');
-      }
-    }
-    if (!radios.length) continue;
-    let isValue = [...radios].some(radios => radios.checked) //一開始還在想到底要怎麼要才取的到布林值，卡關在這裡
-    if (isValue) {
-      let r = ele.querySelector('input[type=radio]:checked');
-      let radioRes = r.nextElementSibling.innerText;
-      res[r.name] = radioRes;
-      ele.classList.add('hide-error');
-    } else {
-      ele.classList.remove('hide-error');
+      res[input.name] = input.value;
+    } if (!input.value) {
+      input.closest('.required').classList.remove('hide__err');
       hasErr = true;
+    } else {
+      input.closest('.required').classList.add('hide__err');
+    }
+  }
+  // 報名類型一開始怎樣都寫不對，看了檢討影片學了 some()
+  const radios = document.querySelectorAll('input[type=radio]');
+  for (const radio of radios) {
+    if ([...radios].some((radio) => radio.checked)) {
+      radio.closest('.required').classList.add('hide__err');
+      res[radio.type] = radio.nextSibling.innerText;
+    } else {
+      radio.closest('.required').classList.remove('hide__err');
     }
   }
 
-})
-
+  const other = document.querySelector('.other');
+  if (other.value) res[other.name] = other.value;
+  if (!hasErr) {
+    alert(`
+    暱稱：${res.name}
+    電子郵件：${res.email}
+    手機號碼：${res.phone}
+    報名類型：${res.radio}
+    怎麼知道這個活動的：${res.referal}
+    其他 對活動的一些建議：${res.other}
+  `);
+  }
+});
